529684983072dcf2076590edddc91a98b0d6b865
unidesk <vinicius.nogueira@unidesk.com.br>
2022-11-24 11:06:18 -0300

    Componente Row (linhas) verticais infinitas

--- app/src/main/java/projetkotlin/com/corecomposeaplication/MainActivity.kt
+++ app/src/main/java/projetkotlin/com/corecomposeaplication/MainActivity.kt
@@ -3,17 +3,14 @@ package projetkotlin.com.corecomposeaplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +25,23 @@ class MainActivity : ComponentActivity() {
 fun MainSreen() {
     Surface(
         color = Color.DarkGray,
-        modifier = Modifier.fillMaxSize()) {
-        Surface(
-            color = Color.Magenta,
-            modifier = Modifier.wrapContentSize(
-                align = Alignment.BottomStart)) {
-            Text(
-                text = "Wrap content",
-                style = MaterialTheme.typography.headlineLarge)
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row {
+            Surface(
+                color = Color.Magenta,
+                modifier = Modifier
+                    .height(600.dp)
+                    .width(60.dp)
+            ) {
+            }
+            Surface(
+                color = Color.Red,
+                modifier = Modifier
+                    .height(600.dp)
+                    .width(60.dp)
+            ) {
+            }
         }
     }
 }
